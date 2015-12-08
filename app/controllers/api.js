6472d31784d6b5f0fefa0dd21dec0b51e5655e8b
Stephanie Miranda <stephanieMiranda@Stephanies-MacBook-Pro.local>
2015-12-07 23:14:29 -0600

    maps displays with marker, autocomplete works

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -5,16 +5,16 @@ app.controller("apiController", ["$scope", "$window", "$firebaseArray",
 
 	var from;
 	var to;
-	var map = null;
+	var map;
 	var mapOptions = {}
 	var geocoder;
+	var address;
+	var marker; 
 	//setting global latitude and longitud. Deafult to Nashville.
 	var myLatLng = {lat: 36.166361, lng: -86.781167};
 
 	//initializing the map. Deafult to Nashville
 	$window.initMap = function () {
-		//to move marker posisiton.
-		// geocoder = new google.maps.Geocoder();
 		//deafult for map
 		mapOptions =  {
 	    center: myLatLng,
@@ -22,41 +22,61 @@ app.controller("apiController", ["$scope", "$window", "$firebaseArray",
 	  }
 	 	//creates a map inside the map div
 		map = new google.maps.Map(document.getElementById('map'), mapOptions);
+		//marker deafult
+		marker = new google.maps.Marker({
+		    position: myLatLng,
+		    map: map,
+		    title: 'Hello World!'
+		  });
 
+
+	};
+
+	//changin the marker position
+	$scope.geocodeAddress = function (geocoder, map) {
+		console.log("resultsMap", map);
+		
+		// geocoder = new $window.google.maps.Geocoder();
+
+		geocoder.geocode( { 'address': address}, function(results, status) {
+	      if (status == google.maps.GeocoderStatus.OK) {
+	        map.setCenter(results[0].geometry.location);
+	        mapOptions = {
+	            map: map,
+	            position: results[0].geometry.location,
+	            zoom: 8,
+	            mapTypeId: google.maps.MapTypeId.ROADMAP
+	        }
+	      } else {
+	        alert("Geocode was not successful for the following reason: " + status);
+	      }
+	    });
 	};
 
 
 	//setting autocomplete function for input field.
 	$scope.placesFrom = function () {
-		//get the html element input
+		//get the html element input for the autocomplete
 		var input = document.getElementById('from');
 		console.log("input", input);
-		//getting the actual address.
-		var address = document.getElementById('from').value;
+		//getting the input string as address.
+		address = document.getElementById('from').value;
 		console.log("address", address);
-		mapOptions = new google.maps.Marker({
-			center: myLatLng,
-			zoom: 8,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		});
 
-		// geocoder( { 'address': address}, function(results, status) {
-	 //      if (status == google.maps.GeocoderStatus.OK) {
-	 //        map.setCenter(results[0].geometry.location);
-	 //        mapOptions = new google.maps.Marker({
-	 //            map: map,
-	 //            position: results[0].geometry.location
-	 //        });
-	 //      } else {
-	 //        alert("Geocode was not successful for the following reason: " + status);
-	 //      }
-	 //    });
+		mapOptions = {
+			position: myLatLng,
+    		map: map,
+		}
 		//create autocomplete object.
-		var autocomplete = new $window.google.maps.places.Autocomplete(input, mapOptions);
+		var autocomplete = new $window.google.maps.places.Autocomplete(input, marker);
 		console.log("autocomplete", autocomplete);
 
-			
 	};//end of placesFrom function
+
+	
+
+
+
 
 		$scope.placesTo = function () {
 		//get the html element input
