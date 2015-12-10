e91db2e9271e178825353c10d09f085084f99395
Stephanie Miranda <stephanieMiranda@Stephanies-MacBook-Pro.local>
2015-12-10 15:57:23 -0600

    user auth works and saves to firebase

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,5 +1,35 @@
-app.controller("loginController", ["$scope", "$firebaseArray", 
-	function ($scope, $window, $firebaseArray) { 
+app.controller("loginController", ["$scope", "$firebaseArray", "$firebaseAuth", "$location",
+	function ($scope, $firebaseArray, $firebaseAuth, $location) { 
+		
 
-	
+
+	$scope.facebookLogin = function() {
+		//firebase reference to app url.
+		var ref = new Firebase("https://commutealert.firebaseio.com/users");
+		//auth reference to firebaseAuth.
+		var auth = $firebaseAuth(ref);	
+
+		auth.$authWithOAuthPopup("facebook").then(function (authData){
+  			console.log("logged in as:", authData.uid);
+        	//setting data inside current user.
+        	ref.child(authData.uid).set(authData);
+        	//if the auth is successfull, relocate to the home url.
+  			$location.url("/commuteAlert/home");
+
+  			}).catch(function(error){
+  				console.log("authentication failed:", error);
+  				});
+  		
+
+  		};//end of facebookLogin
+
+
+
+
+
 }]);//end of controller
+
+
+
+
+
