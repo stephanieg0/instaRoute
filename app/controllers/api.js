54bf169c758868247e343c61637d560a6f9f7f5a
Stephanie Miranda <stephanieMiranda@Stephanies-MacBook-Pro.local>
2015-12-09 21:40:42 -0600

    distance matrix is working

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -1,19 +1,32 @@
 app.controller("apiController", ["$scope", "$window", "$firebaseArray", 
 	function ($scope, $window, $firebaseArray) { 
 
-	console.log("controller is linked");
-
+	//variables from input text in html.	
 	var from = "";
 	var to = "";
-	var map = "";
+	//variables for google instances and objects.
+	$scope.map = "";
 	var mapOptions = {}
-	var geocoder = "";
+	$scope.geocoder = "";
+	$scope.service = "";
+	//variable to get whole address for geocoder.
 	var address = "";
-	var marker = ""; 
-	//setting global latitude and longitud. Deafult to Nashville.
+	//array to have more than one marker who up in the map.
+	$scope.markersArray = []; 
+	//setting global latitude and longitud. Deafult to Nashville.Needed to show map.
 	var myLatLng = {lat: 36.166361, lng: -86.781167};
+	//variables for google's distance matrix.
+	//my origin in address form and coordinates.
+	var origin1 = {lat: 55.93, lng: -3.118};
+  	var origin2 = 'Greenwich, England';
+  	//my destination in address form and coordinates.
+  	var destinationA = 'Stockholm, Sweden';
+  	var destinationB = {lat: 50.087, lng: 14.421};
 
-	//initializing the map. Deafult to Nashville
+  	$scope.calculationRoute = "";
+
+	//initializing the map when app loads(part of the script tag in html). 
+	//Deafult to Nashville
 	$window.initMap = function () {
 		//deafult for map
 		mapOptions =  {
@@ -23,59 +36,58 @@ app.controller("apiController", ["$scope", "$window", "$firebaseArray",
 	 	//creates a map inside the map div
 		map = new google.maps.Map(document.getElementById('map'), mapOptions);
 		//marker deafult
-		marker = new google.maps.Marker({
+		$scope.markersArray.push(new google.maps.Marker({
 		    position: myLatLng,
 		    map: map,
 		    title: 'Hello World!'
-		  });
+		  }));
 
 		//geocoder instance for making address into coordinates.
 		geocoder = new google.maps.Geocoder();
+		//distance matrix instance to get time from origin to destination.
+		service = new google.maps.DistanceMatrixService;
 		
 	};
 
 	//setting autocomplete function for input field.
-	$scope.placesFrom = function (keyEvent) {	
+	$scope.placesFrom = function (keyEvent) {		
 		//get the html element input for the autocomplete
 		var input = document.getElementById('from');
-		// console.log("input", input);
-		//deafult bounds to deafult to places in Nashville first.
-		mapOptions = {
-			position: myLatLng,
-    		map: map,
-		}
+	
 		//create autocomplete object.
-		var autocomplete = new $window.google.maps.places.Autocomplete(input, marker);
+		var autocomplete = new $window.google.maps.places.Autocomplete(input, $scope.markersArray);
 
 		//Get complete address on enter key.
 		if (keyEvent.which === 13) {
-			//getting the input string as address.
+		$scope.markersArray.pop();	
+			//getting the input string as address for geocoder.
 			address = document.getElementById('from').value;
-			console.log("address", address);
+			
+			//passing address to origin2 for distance matrix
+			origin2 = address;
+
 			//passing map object and geocoder instance to function.
 			$scope.geocodeAddress(geocoder, map);
+		
 		}//end if.
-
 	};//end of placesFrom function.
 
 	//setting autocomplete function for input field.
 	$scope.placesTo = function (keyEvent) {
 		//get the html element input
 		var input = document.getElementById('to');
-		console.log("input", input);
-		//deafult bounds to deafult to places in Nashville first.
-		mapOptions = {
-			position: myLatLng,
-    		map: map,
-		}
+		
 		//create autocomplete object.
-		var autocomplete = new $window.google.maps.places.Autocomplete(input, marker);
+		var autocomplete = new $window.google.maps.places.Autocomplete(input, $scope.markersArray);
 
 		//Get complete address on enter key.
-		if (keyEvent.which === 13) {
-			//getting the input string as address.
+		if (keyEvent.which === 13) {			
+			//getting the input string as address for geocoder.
 			address = document.getElementById('to').value;
 			console.log("address", address);
+			//passing address to destinationA for distance matrix.
+			destinationA = address;
+			console.log("destinationA", destinationA);
 			//passing map object and geocoder instance to function.
 			$scope.geocodeAddress(geocoder, map);
 		}//end if.
@@ -89,20 +101,69 @@ app.controller("apiController", ["$scope", "$window", "$firebaseArray",
 		geocoder.geocode( { 'address': address}, function(results, status) {
 	      if (status == google.maps.GeocoderStatus.OK) {
 	        map.setCenter(results[0].geometry.location);
-	        var marker = new google.maps.Marker ({
+	        	$scope.markersArray.push(new google.maps.Marker({
 	            map: map,
 	            position: results[0].geometry.location,
 	            zoom: 8,
 	            mapTypeId: google.maps.MapTypeId.ROADMAP
-	        });
-	      } else {
-	        alert("Geocode was not successful for the following reason: " + status);
-	      }
-
-	    });
-	};
-
-
+	        }));
+	        //setting new position from geocoder for distance matrix variables.
+	    	origin1 = $scope.markersArray[0].position;	        
+	    	
+	    	//setting second array for distance matrix.  	
+	      	} else {
+	        	alert("Geocode was not successful for the following reason: " + status);
+	      			}
+      		if ($scope.markersArray.length === 2) {
+				//setting new position from geocoder for distance matrix variables.
+    			destinationB = $scope.markersArray[1].position;
+    			$scope.GetMatrix();
+				}
+	    	});
+		};
 		
+		//getting new format of the geo code address and running Distance Matrix
+		$scope.GetMatrix = function() {	
+			service.getDistanceMatrix({
+		    origins: [origin1, origin2],
+		    destinations: [destinationA, destinationB],
+		    travelMode: google.maps.TravelMode.DRIVING,
+		    unitSystem: google.maps.UnitSystem.METRIC,
+		    avoidHighways: false,
+		    avoidTolls: false
+		  }, function(response, status) {
+		    if (status !== google.maps.DistanceMatrixStatus.OK) {
+		      alert('Error was: ' + status);
+		    } else {
+		      var originList = response.originAddresses;
+		      var destinationList = response.destinationAddresses;
+		      console.log("originList", originList);
+		      console.log("destinationList", destinationList);
+		      //Defining variables to outPut to dom.
+		      // var outputDiv = document.getElementById('output');
+		      // outputDiv.innerHTML = '';
+		      // deleteMarkers(markersArray);
+		    	}//end of else
+			
+			//Getting the time and distance based on geocode!
+			for (var i = 0; i < originList.length; i++) {
+        		var results = response.rows[i].elements;
+        		geocoder.geocode({'address': originList[i]});
+        		for (var j = 0; j < results.length; j++) {
+          			geocoder.geocode({'address': destinationList[j]});
+          			//Output to the dom
+          	 		//var outputDiv.innerHTML += originList[i] + ' to ' + destinationList[j] +
+           	    	//': ' + results[j].distance.text + ' in ' +
+            	   	//results[j].duration.text + '<br>';
+           			console.log("results[j].duration.text", results[j].duration.text);          			
+       				}
+				}
+			});
+		};//end of function
 
+			
 }]);//end of controller
+
+
+
+
